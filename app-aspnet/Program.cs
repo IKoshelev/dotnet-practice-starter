bf9d580afa4f45693617432bf51899d2b103c27e
IKoshelev <koshelew@live.com>
2023-11-04 01:30:37 +0100

    shortly before dropping seq

--- app-aspnet/Program.cs
+++ app-aspnet/Program.cs
@@ -7,18 +7,16 @@ using Microsoft.Extensions.DependencyInjection;
 using OpenTelemetry.Trace;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Logs;
+using OpenTelemetry.Instrumentation.AspNetCore;
 
-var builder = WebApplication.CreateBuilder(args);
+var webAppBuilder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-// Add services to the container.
-builder.Services.AddRazorPages();
+webAppBuilder.Services.AddControllersWithViews();
+webAppBuilder.Services.AddRazorPages();
 
 JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
 
-builder.Services.AddAuthentication(options =>
+webAppBuilder.Services.AddAuthentication(options =>
     {
         options.DefaultScheme = "Cookies";
         options.DefaultChallengeScheme = "oidc";
@@ -42,110 +40,83 @@ builder.Services.AddAuthentication(options =>
         options.SaveTokens = true;
     });
 
-//builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
+// Build a resource configuration action to set service information.
+Action<ResourceBuilder> configureResource = (r) =>
+{
+
+    r.AddService(
+        serviceName: "WebUI8824",
+        serviceNamespace: "Docker example",
+        serviceVersion: typeof(Program).Assembly.GetName().Version?.ToString() ?? "unknown",
+        serviceInstanceId: Environment.MachineName);
+
+    r.AddAttributes(new Dictionary<string, object>
+    {
+        ["host.name"] = Environment.MachineName,
+        ["deployment.environment"] =
+            webAppBuilder.Environment.EnvironmentName.ToLowerInvariant(),
+    });
+};
+
+// https://github.com/open-telemetry/opentelemetry-dotnet/blob/main/examples/AspNetCore/Program.cs
+webAppBuilder.Services.AddOpenTelemetry()
+    .ConfigureResource(configureResource)
+    .WithTracing(traceProviderBuilder =>
+    {
+        // Tracing
+
+        // Ensure the TracerProvider subscribes to any custom ActivitySources.
+        traceProviderBuilder
+            //.AddSource(Instrumentation.ActivitySourceName)
+            .SetSampler(new AlwaysOnSampler())
+            //.AddHttpClientInstrumentation()
+            .AddAspNetCoreInstrumentation();
+
+        // Use IConfiguration binding for AspNetCore instrumentation options.
+        //webAppBuilder.Services.Configure<AspNetCoreInstrumentationOptions>(appBuilder.Configuration.GetSection("AspNetCoreInstrumentation"));
+
+        traceProviderBuilder.AddOtlpExporter(otlpOptions =>
+        {
+            otlpOptions.Endpoint = new Uri("http://127.0.0.1:4317");
+        });
+
+#if DEBUG
+        traceProviderBuilder.AddConsoleExporter();
+#endif
+    });
+
+webAppBuilder.Logging.ClearProviders();
+//builder.Logging.AddConsole();
+webAppBuilder.Logging.AddOpenTelemetry(options =>
+{
+    var resourceBuilder = ResourceBuilder.CreateDefault();
+    configureResource(resourceBuilder);
+    options.SetResourceBuilder(resourceBuilder);
+
+    //options.IncludeFormattedMessage = true;
+    options.IncludeScopes = true;
+    options.ParseStateValues = true;
+
+    options.AddOtlpExporter(otlpOptions =>
+    {
+        otlpOptions.Endpoint = new Uri("http://127.0.0.1:4317");
+    });
+
+#if DEBUG
+    options.AddConsoleExporter();
+#endif
+});
+
+webAppBuilder.Services.AddControllers();
+webAppBuilder.Services.AddEndpointsApiExplorer();
+webAppBuilder.Services.AddSwaggerGen(options =>
 {
     // using System.Reflection;
     var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 });
 
-// Define attributes for your application
-var openTelemetryBuilder = ResourceBuilder.CreateDefault()
-                                // add attributes for the name and version of the service
-                                .AddService("WebUI8824", "Docker example", "1")
-                                // add attributes for the OpenTelemetry SDK version
-                                .AddTelemetrySdk()
-                                // add custom attributes
-                                .AddAttributes(new Dictionary<string, object>
-                                {
-                                    ["host.name"] = Environment.MachineName,
-                                    ["deployment.environment"] =
-                                        builder.Environment.EnvironmentName.ToLowerInvariant(),
-                                });
-
-
-// openTelemetryBuilder.AddAspNetCoreInstrumentation();
-
-
-builder.Logging.ClearProviders();
-//builder.Logging.AddConsole();
-builder.Logging.AddOpenTelemetry(options =>
-{
-    options
-         // define the resource
-         .SetResourceBuilder(openTelemetryBuilder)
-         // add custom processor
-         //.AddProcessor(new CustomLogProcessor())
-         ;
-
-
-    #if DEBUG
-    options.AddConsoleExporter();
-    #endif
-         
-    options.IncludeFormattedMessage = true;
-    options.IncludeScopes = true;
-    options.ParseStateValues = true;
-
-    options.AddOtlpExporter(exporter => 
-    {
-        exporter.Endpoint = new Uri("http://127.0.0.1:4317"); //jaeger-open-telemetry-logs
-    });
-});
-
-builder.Services
-    .AddOpenTelemetry()
-    .ConfigureResource(c =>
-    {
-        c.AddService("WebUI8824", "Docker example", "1");
-        c.AddAttributes(new Dictionary<string, object>
-        {
-            ["host.name"] = Environment.MachineName,
-            ["deployment.environment"] =
-                builder.Environment.EnvironmentName.ToLowerInvariant(),
-        });
-    })
-    .WithTracing(builder => 
-    {
-        builder.AddAspNetCoreInstrumentation();
-
-        #if DEBUG
-        builder.AddConsoleExporter();
-        #endif
-    
-        builder.AddOtlpExporter(exporter => 
-        {
-            exporter.Endpoint = new Uri("http://127.0.0.1:4317"); //jaeger-open-telemetry-logs
-        });
-    });
-
-// builder.Services.AddOpenTelemetry()
-//             .ConfigureResource(c =>
-//             {
-//                 c.AddService("WebUI8824", "Docker example", "1");
-//                 c.AddAttributes(new Dictionary<string, object>
-//                 {
-//                     ["host.name"] = Environment.MachineName,
-//                     ["deployment.environment"] =
-//                         builder.Environment.EnvironmentName.ToLowerInvariant(),
-//                 });
-//             })
-//             .WithTracing(builder => 
-//             {
-//                 builder.AddAspNetCoreInstrumentation();
-//                 #if DEBUG
-//                 builder.AddConsoleExporter();
-//                 #endif
-
-//                 builder.AddOtlpExporter(exporter => 
-//                 {
-//                     exporter.Endpoint = new Uri("http://127.0.0.1:4317"); //jaeger-open-telemetry-logs
-//                 });
-//             });
-
-var app = builder.Build();
+var app = webAppBuilder.Build();
 
 if (app.Environment.IsDevelopment())
 {
@@ -168,6 +139,7 @@ app.UseAuthorization();
 
 app.MapRazorPages().RequireAuthorization();
 
+//app.MapControllers();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
